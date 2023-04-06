23832f10271f24c306c9ff8ce5572f5d05f5e077
Marcel Arie <googlillo@gmail.com>
2023-04-06 18:02:28 +0200

    build really basic ls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io};
+
+fn main() -> io::Result<()> {
+    let files = fs::read_dir(".")?
+        .map(|res| res.map(|e| e.file_name()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    for file in files {
+        let filename = file.into_string().unwrap();
+        print!("{} ", filename);
+    }
+
+    Ok(())
 }
